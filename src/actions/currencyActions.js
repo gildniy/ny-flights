fe7c1ecb506fd3bdac42027afef854796857a9c6
Gildas Niyigena <gildniy05@gmail.com>
2019-12-22 12:45:08 +0200

    bug(cors) cors fix

--- src/actions/currencyActions.js
+++ src/actions/currencyActions.js
@@ -1,35 +1,35 @@
 import { CONVERT_CURRENCY, THROW_ERROR } from './types'
 import { RAPID_API_KEY } from '../../config/keys'
 
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/'
+const URL = 'https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from='
+
 const convertCurrency = (dispatch, { from, to }) => {
   const { get } = require('axios')
-  get(
-    'https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=' +
-    from + '&to=' + to + '&amount=1',
+  get(PROXY_URL + URL + from + '&to=' + to + '&amount=1',
     {
-      crossDomain: true,
       headers: {
         'Content-Type': 'application/json',
         'x-rapidapi-host': 'currency-converter5.p.rapidapi.com',
         'x-rapidapi-key': RAPID_API_KEY,
       },
     })
-    .then(res => res.data)
-    .then(({ rates }) => {
-      return dispatch({
-        type: CONVERT_CURRENCY,
-        payload: {
-          rate: rates[to]['rate'],
-          name: rates[to]['currency_name'],
-        },
-      })
+  .then(res => res.data)
+  .then(({ rates }) => {
+    return dispatch({
+      type: CONVERT_CURRENCY,
+      payload: {
+        rate: rates[to]['rate'],
+        name: rates[to]['currency_name'],
+      },
     })
-    .catch(error => {
-      return dispatch({
-        type: THROW_ERROR,
-        payload: { error },
-      })
+  })
+  .catch(error => {
+    return dispatch({
+      type: THROW_ERROR,
+      payload: { error },
     })
+  })
 }
 
 export { convertCurrency }
